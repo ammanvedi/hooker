e74be243376bc83687e100f082471a9d4a6f9db2
Amman Vedi <amman.vedi@gmail.com>
2014-02-25 16:52:07 +0000

    loading properties of service loaction from json config file implemented

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var http = require('http');
 var path = require('path');
 var sys = require('sys');
 var exec = require('child_process').exec;
+var fs = require('fs');
 
 function puts(error, stdout, stderr) { sys.puts(stdout) }
 
@@ -42,6 +43,8 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+
+
 app.post('/hook', function (req, res){
 
 //the github server will post data here
@@ -49,6 +52,29 @@ var commit_msg = req.body.commits[0].message;
 var printtext;
 var now = new Date();
 
+
+//get the config variables
+
+var hook_path, hook_branch, hook_exname, hook_logname, hook_repo;
+
+//load them from file
+
+fs.readFile(__dirname + '/public/config/cfg.json', 'utf8', function (err, data) {
+  if (err) {
+    console.log('Error: ' + err);
+    return;
+  }
+ 
+  data = JSON.parse(data);
+  
+  hook_path = data.PATH;
+  hook_branch = data.BRANCHNAME;
+  hook_exname = data.EXNAME;
+  hook_logname = data.LOGNAME;
+  hook_repo = data.GITREPO;
+ 
+  console.log("cd " + hook_path +" && git pull " + hook_repo + " " + hook_branch + " && forever start -o " + hook_logname + " -a " + hook_exname);
+});
 
 if(commit_msg.indexOf("[redeploy]") != -1)
 {
@@ -58,7 +84,7 @@ if(commit_msg.indexOf("[redeploy]") != -1)
 	//run script to redeploy based on preferences jSON
 	
 	
-	exec("cd /home/ubuntu/seeder && git pull seeder v2_arbor && forever restart 1",config, puts);
+	exec("cd " + hook_pathr +" && git pull " + hook_repo + " " + hook_branch + " && forever start -o " + hook_logname + " -a " + hook_exname,config, puts);
 //	exec("git pull seeder v2_arbor",config, puts);
 //	exec("forever restart 1",config, puts);
 	
@@ -71,7 +97,7 @@ if(commit_msg.indexOf("[redeploy]") != -1)
 
 	
 	
-	var fs = require('fs');
+	
 fs.writeFile("hooker.txt", printtext, function(err) {
     if(err) {
         console.log(err);
